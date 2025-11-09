fdb4a4311249d34c47da5ebc467a3dfff30da55a
Jade <thedavidsoto@gmail.com>
2025-11-09 20:03:15 +0800

    Apply solutions to exercises 01 to 05

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,8 +1,8 @@
 function numberChecker(number) {
-  if (number === 6) {
-    return true;
-  } else {
+  if (number === 6 || number === 9) {
     return false;
+  } else {
+    return true;
   }
 }
 
