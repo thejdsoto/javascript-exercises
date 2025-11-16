279a594c8b45b10aa82a31a4ff5e546974214ff8
Jade <thedavidsoto@gmail.com>
2025-11-16 18:07:45 +0800

    Add solution to sumAll problem

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    
+    if (a > b) {
+        [a, b] = [b, a];
+    } else if (a < 0 || b < 0) {
+        return "ERROR";
+    } else if (!(Number.isInteger(a)) || !(Number.isInteger(b))) {
+        return "ERROR";
+    }
 
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
